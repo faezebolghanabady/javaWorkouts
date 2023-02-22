6f55ce78a2beeb5bc8572c89318ba0f1e303401b
faeze <faezeebold@gmail.com>
2023-02-23 00:47:40 +0330

    logical And

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+//تمرینی برای  عملگر ها که در آن چنانچه کاربر پول کافی داشته باشد و محصول نیز وجود داشته باشد 
+//به عبارتی هر دو شرط برقرار باشد مقدار true 
+//وگرنه مقدار false
 function buyProduct( productCount , productPrice , credit){
     
-    let buy = productCount > 0 && productPrice > credit ;
+    let buy = productCount > 0 &&  credit > productPrice ;
     console.log(buy)
 }
 
