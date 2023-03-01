026285875ff1ed8a761fde885f87517096f1e77d
faeze <faezeebold@gmail.com>
2023-03-01 18:39:52 +0330

    find

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//object
-
-const users = {
-    user1 :{
+//find , findIndex
+let users = [
+    {
         name :'faeze',
         family : 'bolghanabady',
         id : 23 ,
     },
-    user2 :{
+    {
         name :'sina',
         family : 'porjalal',
         id : 23 ,
     },
-    user3 :{
+    {
         name :'mahdi',
         family : 'khomarian',
         id : 23 ,
     }
-}
+]
 
-//template
-let admin = `hi ${users.user1.name}  welcome`
-let information = ` other admins ${ users.user2.family } and ${users.user3.family}`
-console.log(admin , information)
+let admin = users.find(function(user){
+    return user.name === 'sina'
+})
+
+console.log(admin)
+
