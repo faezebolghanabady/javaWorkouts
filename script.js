718ab915c339fc2eb8001cdf7bae34aac9117182
faeze <faezeebold@gmail.com>
2023-03-15 00:42:48 +0330

    Array

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //نام کاربر را در آرایه جستوجو کرده درصورت عدم موجودیت کاربر را به ابتدای لیست اضافه نماید
-const users = [ 'mahdi' , 'mobin' , 'mina' , 'sina' , 'ahmad' ]
-if(users.includes('faeze')){
-    console.log('کاربر وجود دارد')
-}else{
-    console.log('کاربر وجود ندارد')
-    users.unshift('faeze')
-}
-console.log( 'لیست جدید' + ':' +users)
+const users = [ 
+    user1 = {
+        name:'faeze',
+        family:'bolghanabady',
+        id:9595
+    },
+    user2 = {
+        name:'mina',
+        family:'bolboly',
+        id:958
+    },
+    user1 = {
+        name:'sina',
+        family:'ahmadi',
+        id:95581
+    },
+    user1 = {
+        name:'nima',
+        family:'hashemy',
+        id:9785
+    },
+    user1 = {
+        name:'ahmad',
+        family:'bolboly',
+        id:959321
+    },
+
+ ]
+
+const resalt = users.find(function(user){
+    return user.id ===9785
+})
+console.log(resalt)
