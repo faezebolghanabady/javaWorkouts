5ea69329289eec68da0bee2db2d39ccc8e67a463
faeze <faezeebold@gmail.com>
2023-05-03 22:18:58 +0330

    array

---
+++ main/moror.js
@@ -0,0 +1,13 @@
+
+const user = {
+    name : 'faeze' ,
+    family : 'bolghanabady',
+}
+
+console.log(user);
+
+const newUser = Object.assign({age : 23} ,user )
+console.log(newUser);
+
+const users = {name : 'mobin' , ...user }
+    console.log(users);
