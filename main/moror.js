b7383b2c36b39592141735eff5f334d054be8f9a
faeze <faezeebold@gmail.com>
2023-05-08 22:29:06 +0330

    test

--- main/moror.js
+++ main/moror.js
@@ -6,7 +6,7 @@ const user = {
 
 console.log(user);
 
-const newUser = Object.assign({age : 29} ,user )
+const newUser = Object.assign({age : 24} ,user )
 console.log(newUser);
 
 const users = {name : 'mobin' , ...user }
